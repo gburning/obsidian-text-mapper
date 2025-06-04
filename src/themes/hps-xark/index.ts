747ed65edca833a609ef01dd731d48a36024ef7d
Gabriel Burning <gabriel.burning@svep.se>
2025-06-04 18:23:58 +0200

    fix: clean up assets

--- src/themes/hps-xark/index.ts
+++ src/themes/hps-xark/index.ts
@@ -1,13 +1,11 @@
 import type { Theme } from "src/lib/theme";
-import imageAssets from "./assets/**/*.png";
-import { getAssetUri } from "../../lib/util";
+import _imageAssets from "src/themes/hps-xark/assets/**/*.png";
+import { flattenAssets, getAssetUri } from "../../lib/util";
 
-const { tiles, icons } = imageAssets as unknown as Record<
-    string,
-    Record<string, string>
->;
+const imageAssets = flattenAssets(
+    _imageAssets as unknown as Record<string, string>
+);
 
-// TODO: Document the widths/heights used (they seem to be required for some reason)
 export const hpsXark: Theme = {
     displayName: "The Tiles of Xark!",
     id: "hps-xark",
@@ -17,31 +15,17 @@ export const hpsXark: Theme = {
             "option coordinates-format {x}.{y}",
             // Defaults
             `default attributes fill="none" stroke="none"`,
-            // Paths
-            `trail path attributes stroke="#e3bea3" stroke-width="6" fill="none"`,
-            `river path attributes transform="translate(20,10)" stroke="#6ebae7" stroke-width="8" fill="none" opacity="0.7"`,
-            `canyon path attributes transform="translate(20,10)" stroke="black" stroke-width="24" fill="none" opacity="0.2"`,
         ];
 
-        // Object.entries(icons).forEach(([key, value]) => {
-        //     // Remove the file extension from the key
-        //     const _key = key.replace(/\.(png|jpg|jpeg|svg)$/, "");
-        //     res.push(
-        //         `<image id="${_key}" href="${getAssetUri(
-        //             value
-        //         )}" height="225" width="225" y="-112.5" x="-112.5" transform="scale(0.8)" />`
-        //     );
-        // });
-
-        // Object.entries(tiles).forEach(([key, value]) => {
-        //     // Remove the file extension from the key
-        //     const _key = key.replace(/\.(png|jpg|jpeg|svg)$/, "");
-        //     res.push(
-        //         `<image id="${_key}" href="${getAssetUri(
-        //             value
-        //         )}" height="225" width="225" y="-112.5" x="-112.5" />`
-        //     );
-        // });
+        imageAssets.forEach((asset) => {
+            // Remove the file extension from the key
+            const _key = asset.name.replace(/\.(png|jpg|jpeg|svg)$/, "");
+            res.push(
+                `<image id="hps-x-${_key}" href="${getAssetUri(
+                    asset.content
+                )}" height="225" width="225" y="-112.5" x="-112.5" />`
+            );
+        });
 
         return res;
     },
