f89953d30a6fd710a1128fef5a831ba9ad61892f
Gabriel Burning <gabriel.burning@svep.se>
2025-06-04 21:41:45 +0200

    fix: conflicting glob patterns

--- src/themes/hps-doodle/index.ts
+++ src/themes/hps-doodle/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from "../../lib/theme";
 import { getAssetUri, getElementIdFromPath } from "../../lib/util";
-import { modules as assets } from "./assets/**/*.png#default";
+// ../ is required as the glob plugin will see it as conflicting with other, identical, patterns
+import { modules as assets } from "../hps-doodle/assets/**/*.png#default";
 
 export const hpsDoodle: Theme = {
     displayName: "HPS Doodle Tiles",
--- src/themes/hps-xark/index.ts
+++ src/themes/hps-xark/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from "../../lib/theme";
 import { getAssetUri, getElementIdFromPath } from "../../lib/util";
-import { modules as assets } from "./assets/**/*.png#default";
+// ../ is required as the glob plugin will see it as conflicting with other, identical, patterns
+import { modules as assets } from "../hps-xark/assets/**/*.png#default";
 
 export const hpsXark: Theme = {
     displayName: "The Tiles of Xark!",
